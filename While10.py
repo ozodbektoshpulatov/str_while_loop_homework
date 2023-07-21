74c7ecf695111e9173fbf196c1c1821d7c317f32
ozodbektoshpulatov <toshpulatovozodbek96@gmail.com>
2023-07-21 09:58:26 +0500

    added solutions

--- While10.py
+++ While10.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    ind=0
+    ans=0
+    while ind<len(s):
+        if s[ind] in "13579":
+            ans+=int(s[ind])
+        ind+=1
+    return ans
+print(main("1abcd7uj8"))

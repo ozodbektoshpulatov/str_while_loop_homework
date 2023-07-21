01366e141c6f68c725bfd59e73c7603f0f712d7c
ozodbektoshpulatov <toshpulatovozodbek96@gmail.com>
2023-07-21 06:36:39 +0500

    05

--- While05.py
+++ While05.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    ind=0
+    ans=0
+    while ind<len(s):
+        if s[ind] in "asdfghjkmnbvcxzfsvkfmhdbcjfg":
+            ind+=1
+        ans+=1
+    return ans
+print(main("bfgdnjddkn"))
